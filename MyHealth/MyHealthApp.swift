bb4e431ec21225dec2b31ce6b62ef6d2b9e37b29
zhanghengshan <zhanghengshan@manjitech.com>
2023-07-06 09:17:24 +0800

    Initial Commit

---
+++ MyHealth/MyHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthApp.swift
+//  MyHealth
+//
+//  Created by zhs on 2023/7/6.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
